cf5ecdc91387fd180a8d1354e743a2c2cac37f9f
Marco Granatiero <marco.granatiero@3wlab.it>
2014-09-12 14:27:47 +0200

    separate library from demo module

--- demo/src/main/java/it/moondroid/colormixerdemo/demo/FragmentActivity.java
+++ demo/src/main/java/it/moondroid/colormixerdemo/demo/FragmentActivity.java
@@ -1,20 +1,19 @@
-package it.moondroid.colormixer;
+package it.moondroid.colormixerdemo.demo;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.SeekBar;
+
+import it.moondroid.colormixer.HSLFragment;
 
 
-public class MainActivity extends ActionBarActivity implements HSLFragment.OnColorChangeListener {
+public class FragmentActivity extends ActionBarActivity implements HSLFragment.OnColorChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
 
     }
 
